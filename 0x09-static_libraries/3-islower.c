27064bd315b6cee39f55428c976d26d5a627d985
orjimiracle1 <orjimiraclealx@gmail.com>
2023-10-02 20:56:25 -0700

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - funtion that checks if an alphabet is lowercase
+ * @c: the char holder
+ * Return: 1 for success
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
