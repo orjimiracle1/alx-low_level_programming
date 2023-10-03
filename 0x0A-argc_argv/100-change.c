d469711416bf1a7114d2f2fe3b1f9b6e6626a46a
orjimiracle1 <orjimiraclealx@gmail.com>
2023-10-02 19:51:29 -0700

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /**
- * main - The function execution begins
+ * main - Entry point
  * @argc: The argument count
  * @argv: The argument vector
  * Return: 0 for success
@@ -11,26 +12,29 @@ int main(int argc, char *argv[])
 {
 	int coins_array[] = {25, 10, 5, 2, 1};
 	int i;
-	int cent = 0, size_of_array, count;
+	int cent = 0;
+	int size_of_array;
+	int count = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(*(argv + 1)) < 0)
+
+	if (atoi(argv[1]) < 0)
 	{
 		printf("0\n");
 	}
 	else
 	{
-		cent = atoi(*(argv + 1));
-		size_of_array = sizeof(coins_array) / sizeof(*(coins_array + 0));
+		cent = atoi(argv[1]);
+		size_of_array = sizeof(coins_array) / sizeof(coins_array[0]);
 		for (i = 0; i < size_of_array; i++)
 		{
-			while (cent >= *(coins_array + i))
+			while (cent >= coins_array[i])
 			{
-				cent -= *(coins_array + i);
+				cent -= coins_array[i];
 				count++;
 			}
 			if (cent == 0)
@@ -38,5 +42,7 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", count);
 	}
+
 	return (0);
 }
+
